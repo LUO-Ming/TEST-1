19c4357d88f98eb8d7162d11cfc0e09fbb54fe23
faedeveloper <h.ren@innox-jp.com>
2017-04-07 17:12:56 +0900

    edit #00001
    仕様変更

--- HelloWorld_test1.java
+++ HelloWorld_test1.java
@@ -4,6 +4,8 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World modified by LUOs !");
+        System.out.println("Hello World 223 !");
+    	System.out.println("This is a test program!");
     }
+
 }
